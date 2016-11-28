e4f759c150a12a5c3606b2cf9975661009a5ac3e
Raúl Molina <raulmolina88@gmail.com>
2016-11-28 18:52:37 +0100

    complete test with files, removed v4 and 0.12 for the moment, updated to 0.0.3

--- test/app.js
+++ test/app.js
@@ -6,6 +6,7 @@ var helpers = require('yeoman-test');
 describe('generator-angular-es-6:app', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({someAnswer: true})
       .toPromise();
   });
 
@@ -15,7 +16,23 @@ describe('generator-angular-es-6:app', function () {
       'package.json',
       'gulpfile.js',
       'app/app.js',
-      'app/assets/vendor/normalize.css'
+      'app/assets/vendor/normalize.css',
+      'app/assets/sass/custom.scss',
+      'app/components/index.js',
+      'app/config/app.config.js',
+      'app/config/app.run.js',
+      'app/config/app.constants.js',
+      'app/home/home.config.js',
+      'app/home/home.controller.js',
+      'app/home/index.js',
+      'app/home/home.html',
+      'app/layout/app-view.html',
+      'app/layout/header.html',
+      'app/layout/footer.html',
+      'app/layout/header.component.js',
+      'app/layout/footer.component.js',
+      'app/layout/index.js',
+      'app/services/index.js'
     ]);
   });
 });
